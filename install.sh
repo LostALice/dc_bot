e0906534af65a6605cfb0408a8ad8713ddb45c75
LostALice <63731417matthew@gmail.com>
2021-12-13 19:19:23 +0800

    updated install.sh with docker tag

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
-docker image build -t dc_bot .
+now=$(date +"%s")
+
+docker image build -t dc_bot:$now .
 
 echo -e "\e[1;35m Build successfully \e[0m"
 
-docker run -d -v /home/config/dc_bot/mp3:/discord/mp3 --name dc_bot dc_bot
+docker run -d -v /home/config/dc_bot/mp3:/discord/mp3 --name dc_bot dc_bot:$now
 
 echo -e "\e[1;35m Added container \e[0m"
