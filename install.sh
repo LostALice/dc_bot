e3f042683b520c1634026dc5db5b62a60aed485a
LostALice <63731417matthew@gmail.com>
2021-12-19 01:55:53 +0800

    delete useless sth

--- install.sh
+++ install.sh
@@ -3,7 +3,3 @@ now=$(date +"%s")
 docker image build -t dc_bot:$now .
 
 echo -e "\e[1;35m Build successfully \e[0m"
-
-docker run -d -v /home/config/dc_bot/mp3:/discord/mp3 --name dc_bot dc_bot:$now
-
-echo -e "\e[1;35m Added container \e[0m"
